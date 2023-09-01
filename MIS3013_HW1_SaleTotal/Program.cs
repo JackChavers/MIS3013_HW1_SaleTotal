c0df2f8b293a3dd46167a5b5573d98ff4974dfc9
jackc <jackchavers7@gmail.com>
2023-09-01 14:28:38 -0500

    Add project files.

---
+++ MIS3013_HW1_SaleTotal/Program.cs
@@ -0,0 +1,34 @@
+﻿// MIS 3013 HW1
+// sales total
+
+string outMesStr = "What is the name of the product you are purchasing? ";
+Console.Write(outMesStr);
+string productNameStr = Console.ReadLine();
+
+outMesStr = string.Format($"How many {productNameStr}'s do you want to buy? ");
+Console.Write(outMesStr);
+string quanStr = Console.ReadLine();
+double quanDbl = Convert.ToDouble(quanStr);
+
+outMesStr = string.Format($"What is the price of {productNameStr}? ");
+Console.Write(outMesStr);
+string priceStr = Console.ReadLine();
+double priceDbl = Convert.ToDouble(priceStr);
+
+double subtotalDbl = quanDbl * priceDbl;
+
+double taxRateDbl = 8.5 / 100;
+
+double salesTaxDbl = subtotalDbl * taxRateDbl;
+
+double totalDbl = subtotalDbl + salesTaxDbl;
+
+outMesStr = string.Format($"\tYour subtotal for the bill is {subtotalDbl:C2}");
+Console.WriteLine(outMesStr);
+
+outMesStr = string.Format($"Your sales tax is {salesTaxDbl:C2}");
+Console.WriteLine(outMesStr);
+
+outMesStr = string.Format($"\tYour total for your bill is {totalDbl:C2}");
+Console.WriteLine(outMesStr);
+
